d9a0dfeb450f93c47270cd9f007babe3253e402a
Amir Kaplan <amirkap7@gmail.com>
2024-03-13 19:14:20 +0200

    Bug fix, added experiment function

--- src/accuracy_assesment.py
+++ src/accuracy_assesment.py
@@ -5,11 +5,15 @@ from connect_to_openai import OpenAIClass, get_assemsment_sys_promompt, get_asse
 from connect_to_spotify import SpotifyAPIClass
 
 FILENAME = 'streamlit.csv'
+NUM_EXPERIMENTS = 3
+audio_features = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness',
+                  'instrumentalness', 'liveness', 'valence', 'tempo']
 
 
 def read_csv_file(filename):
     df = pd.read_csv(filename)
     return df
+
 
 def read_random_track_uris(df, num_rows):
     # Extract the 'track_uri' column
@@ -27,48 +31,58 @@ def read_random_track_uris(df, num_rows):
     # Return the sampled track URIs as a DataFrame
     return pd.DataFrame({'track_uri': sampled_track_uris})
 
-def get_avg_(audio_features_list, spotify, uris_list):
-    pass
 
-# Example usage
-streamlit_df = read_csv_file(FILENAME)  # Read the entire CSV file
-spotify = SpotifyAPIClass()
+def experiment():
+    # Example usage
+    streamlit_df = read_csv_file(FILENAME)  # Read the entire CSV file
+    spotify = SpotifyAPIClass()
 
-# Sample 10,000 random track URIs
-num_rows = 15  # Number of random track IDs to extract
-uri_df = read_random_track_uris(streamlit_df, num_rows)
-print(uri_df)
-names_list = [spotify.get_track_and_artist_name(track_uri) for track_uri in uri_df['track_uri']]
-print(names_list)
+    # Sample 10,000 random track URIs
+    num_rows = 15  # Number of random track IDs to extract
+    uri_df = read_random_track_uris(streamlit_df, num_rows)
+    print(uri_df)
+    names_list = [spotify.get_track_and_artist_name(track_uri) for track_uri in uri_df['track_uri']]
+    print(names_list)
 
+    # Set up OpenAI API
+    openai = OpenAIClass()
+    print("#################### OPENAI RESPONSE ####################")
+    response = openai.get_chat_response_from_openai(get_assemsment_sys_promompt(),
+                                                    get_assesment_user_prompt(names_list))
+    print(response)
+    audio_features_list = ast.literal_eval(response)
+    features_df = pd.DataFrame(audio_features_list)
+    shared_columns = [col for col in features_df.columns if col in streamlit_df.columns]
+    print(f"Shared columns: {shared_columns}")
+    features_df = features_df[shared_columns]
+    uri_df = uri_df.reset_index(drop=True)
+    features_df = features_df.reset_index(drop=True)
+    features_df = pd.concat([uri_df, features_df], axis=1)
+    print(f"Predicted features:\n{features_df}")
+    true_features_df = streamlit_df[streamlit_df['track_uri'].isin(uri_df['track_uri'])]
+    true_features_df = true_features_df[shared_columns + ['track_uri']]
+    print(f"True features:\n{true_features_df}")
+    merged_df = pd.merge(features_df, true_features_df, on='track_uri', suffixes=('_predicted', '_true'))
 
-# Set up OpenAI API
-openai = OpenAIClass()
-print("#################### OPENAI RESPONSE ####################")
-response = openai.get_chat_response_from_openai(get_assemsment_sys_promompt(), get_assesment_user_prompt(names_list))
-print(response)
-audio_features_list = ast.literal_eval(response)
-features_df = pd.DataFrame(audio_features_list)
-features_df = pd.concat([uri_df, features_df], axis=1)
-print(features_df)
-shared_columns = [col for col in features_df.columns if col in streamlit_df.columns and col != 'track_uri']
-print(shared_columns)
-true_features_df = streamlit_df[streamlit_df['track_uri'].isin(uri_df['track_uri'])]
-true_features_df = true_features_df[shared_columns]
-merged_df = pd.merge(features_df, true_features_df, on='track_uri', suffixes=('_predicted', '_true'))
-print(merged_df)
+    diffs = {}
+    for col in shared_columns:
+        diffs[col + '_diff'] = np.mean(abs(merged_df[col + '_predicted'] - merged_df[col + '_true']))
 
-squared_diffs = {}
-for col in shared_columns:
-    squared_diffs[col + '_squared_diff'] = np.mean((merged_df[col + '_predicted'] - merged_df[col + '_true']) ** 2)
-squared_diffs_df = pd.DataFrame(squared_diffs)
-print(squared_diffs_df)
-
-
-
-
+    return diffs
 
 
+totals = {feature + '_diff': 0 for feature in audio_features}
+for i in range(NUM_EXPERIMENTS):
+    print(f"###################### Experiment {i + 1} #######################")
+    avg_diffs = experiment()
+    filtered_avg_diffs = {feature_diff: avg_diffs[feature_diff] for feature_diff in totals.keys()}
+    print(f"Experiment {i + 1} average differences: {filtered_avg_diffs}")
+    for feature in totals:
+        totals[feature] += filtered_avg_diffs[feature]
+    print("\n\n\n")
+
+print(f"###################### AVERAGE DIFFERENCES #######################")
+print({feature: totals[feature] / NUM_EXPERIMENTS for feature in totals})
 
 # print("#################### SPOTIFY API ####################")
 # print([spotify.get_track_features(track_uri) for track_uri in uris_list])
