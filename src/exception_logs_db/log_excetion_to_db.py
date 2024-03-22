28e336d04fdd3afc3d774431b84a3f27fb3cb7e9
Sahar Blueshtein <blueshtein.sahar@post.runi.ac.il>
2024-03-22 16:14:11 +0200

    Added documentation to the new classes

--- src/exception_logs_db/log_excetion_to_db.py
+++ src/exception_logs_db/log_excetion_to_db.py
@@ -7,6 +7,15 @@ from dotenv import load_dotenv
 import os
 
 def log_exception_to_db(level, message):
+    """
+    Log an exception to the database.
+    Args:
+        level: The level of the exception (e.g. ERROR, WARNING, INFO)
+        message: The message of the exception
+
+    Exceptions:
+        Error: An error occurred logging the exception to the database
+    """
     israel_tz = pytz.timezone('Israel')
     now = datetime.now(israel_tz).strftime('%Y-%m-%d %H:%M:%S')
 
@@ -32,6 +41,13 @@ def log_exception_to_db(level, message):
         print(f"Error logging exception to database: {e}")
 
 def get_exception_logs():
+    """
+    Get all the exception logs from the database.
+    Returns:
+        A list of all the exception logs
+    Exceptions:
+        Error: An error occurred getting the exception logs from the database
+    """
     load_dotenv()
     host = os.getenv('MYSQL_HOST')
     user = os.getenv('MYSQL_USERNAME')
--- src/exception_logs_db/logger.py
+++ src/exception_logs_db/logger.py
@@ -3,6 +3,9 @@ from src.exception_logs_db.log_excetion_to_db import log_exception_to_db
 
 
 class MySQLLoggingHandler(logging.Handler):
+    """
+    Custom logging handler to log exceptions to a MySQL database.
+    """
     def emit(self, record):
         # Use the log_exception_to_db function to log the record
         log_exception_to_db(record.levelname, self.format(record))
